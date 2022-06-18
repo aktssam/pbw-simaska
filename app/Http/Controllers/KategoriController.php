fd1ba9877d8ee2b2e0e0c8268ef404ab55d94b51
aktssam <aktssam.alfarisi@gmail.com>
2022-06-19 01:22:25 +0700

    add controllers

---
+++ app/Http/Controllers/KategoriController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Kategori;
+use Illuminate\Http\Request;
+
+class KategoriController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $kategori = Kategori::all();
+        return view('kategori.index', compact('kategori'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('kategori.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'kode_kategori' => 'bail|required|unique:kategori|min:4',
+            'nama_kategori' => 'required|max:255',
+        ]);
+
+        Kategori::create($validated);
+
+        return redirect('kategori')->with('success', 'Berhasil menambahkan data');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Kategori  $kategori
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Kategori $kategori)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Kategori  $kategori
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Kategori $kategori)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Kategori  $kategori
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Kategori $kategori)
+    {
+        //
+    }
+}
