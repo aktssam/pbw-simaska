976525c4d6c04fdf1f3f46d4fc826076a1113284
aktssam <aktssam.alfarisi@gmail.com>
2022-06-19 01:21:16 +0700

    add database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,5 +20,43 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        /* ===== Seeder Kategori ===== */
+        \App\Models\Kategori::create([
+            'kode_kategori' => 'KR02',
+            'nama_kategori' => 'Sepeda Motor',
+        ]);
+
+        \App\Models\Kategori::create([
+            'kode_kategori' => 'KR03',
+            'nama_kategori' => 'Motor Tossa',
+        ]);
+
+        \App\Models\Kategori::create([
+            'kode_kategori' => 'KR04',
+            'nama_kategori' => 'Mobil',
+        ]);
+
+
+        /* ===== Seeder Departemen ===== */
+        \App\Models\Departemen::create([
+            'kode_departemen' => 'DPT001',
+            'nama_departemen' => 'Keuangan',
+        ]);
+
+        \App\Models\Departemen::create([
+            'kode_departemen' => 'DPT002',
+            'nama_departemen' => 'Pemasaran',
+        ]);
+
+        \App\Models\Departemen::create([
+            'kode_departemen' => 'DPT003',
+            'nama_departemen' => 'Produksi',
+        ]);
+
+        \App\Models\Departemen::create([
+            'kode_departemen' => 'DPT004',
+            'nama_departemen' => 'Gudang',
+        ]);
     }
 }
